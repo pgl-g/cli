658c55ca54777989b99ef73636a0b84c231b5bcb
彭格列 <1801688332@qq.com>
2023-08-21 18:29:48 +0800

    update: token

--- config/index.js
+++ config/index.js
@@ -11,4 +11,4 @@ exports.baseUrl = 'https://api.github.com';
 
 // 组织名称
 exports.repName = 'pgl-cli';
-exports.token = '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'
+exports.token = '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'
