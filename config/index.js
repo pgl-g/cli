0413a3973cbda4333d4557a70fd83c8f89cce57f
彭格列 <1801688332@qq.com>
2023-08-21 22:10:57 +0800

    feat: update token

--- config/index.js
+++ config/index.js
@@ -11,4 +11,4 @@ exports.baseUrl = 'https://api.github.com';
 
 // 组织名称
 exports.repName = 'pgl-cli';
-exports.token = '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'
+exports.token = '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'
