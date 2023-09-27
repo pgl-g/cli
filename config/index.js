b2a6e50ae6c06a6d188a07c709586ad904c973e8
guxiaobiao <guxiaobiao@xinxuan.net>
2023-09-27 10:39:02 +0800

    tag: change

--- config/index.js
+++ config/index.js
@@ -11,4 +11,4 @@ exports.baseUrl = 'https://api.github.com';
 // github - setting - developer settings - personal access tokens - fine-grained tokens 
 // 组织名称 === token name
 exports.repName = 'pgl-cli';
-exports.token = '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'
+exports.token = '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'
